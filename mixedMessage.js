2b2a091d741a4c8b9e41411d0f36d812ad34f24a
Michael <mtse431@gmail.com>
2023-07-14 22:26:21 +1000

    Decides Topic and its Purpose

---
+++ mixedMessage.js
@@ -0,0 +1,8 @@
+/* 
+    Project Topics : Chinese Zodiac Astrology
+        1. Abstract
+            Chinese Zodiac consists of 12 animals and 5 elements. Every animal and element has its own personal traits. By knowing your zodiac, you will have more insight about your true self hence be able to control your future success in career, love and life.
+        
+        2. Purpose
+            In this project, everytime users start the program, they will be given a random chinese zodiac sign as well as the element. Base on this zodiac, users will know about their personal trait and will be given a random piece of advice to make them feel better.
+*/
